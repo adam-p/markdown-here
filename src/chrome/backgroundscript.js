b80e374fdfc7b3b3d8e42c5e05a33435aa61a39e
Adam Pritchard <pritchard.adam@gmail.com>
2012-05-21 01:11:29 -0400

    Tighted Chrome extension security by using newer manifest version and using a tight content_security_policy. Necessitated moving background script from html to js.

---
+++ src/chrome/backgroundscript.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright Adam Pritchard 2012
+ * MIT License : http://adampritchard.mit-license.org/
+ */
+
+/*
+ * Chrome background script.
+ */
+
+// Create the context menu that will signal our main code.
+chrome.contextMenus.create({
+  contexts: ['editable'],
+  title: 'Markdown Toggle',
+  onclick: function(info, tab) {
+    chrome.tabs.sendRequest(tab.id, {action: 'context-click'});
+  }
+});
+
+// Handle rendering requests from the content script.
+// See the comment in markdown-render.js for why we do this.
+chrome.extension.onRequest.addListener(
+  function renderRequest(html, sender, sendResponse) {
+    sendResponse({
+      html: markdownRender(htmlToText, marked, html),
+      css: markdownHereCss
+    });
+  });
