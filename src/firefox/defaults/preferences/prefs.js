2099cd8d0ead7584afa572c21c10fe474b63afec
Adam Pritchard <pritchard.adam@gmail.com>
2012-08-18 10:44:55 -0400

    Removed a little test code

--- src/firefox/defaults/preferences/prefs.js
+++ src/firefox/defaults/preferences/prefs.js
@@ -1,2 +1,2 @@
 pref("services.sync.prefs.sync.extensions.markdown-here.markdown-here-css", true);
-pref("services.sync.prefs.sync.extensions.markdown-here.markdown-here-syntax-css", "true");
+pref("services.sync.prefs.sync.extensions.markdown-here.markdown-here-syntax-css", true);
