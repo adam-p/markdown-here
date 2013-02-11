a673acadf3a9ffe670c2831ea4b7e19a2dc9f4f2
Adam Pritchard <pritchard.adam@gmail.com>
2013-02-11 06:56:44 -0500

    Tweaked test runner to not give red herring error

--- src/common/test/test-run.js
+++ src/common/test/test-run.js
@@ -1,1 +1,7 @@
-mocha.run();
+$(function(){
+  mocha
+    // I'm not sure what introduces the global "schemaTypes", but it's not
+    // Markdown Here and it causes an error on one of my Chrome instances.
+    .globals([ 'schemaTypes' ]) // acceptable globals
+    .run();
+});
