d16a0d9fef154d7ddd3d5c5b4caa22d01033965f
Adam Pritchard <pritchard.adam@gmail.com>
2018-09-11 21:21:39 -0400

    Add prompt to help old-Thunderbird users find open-options menu item

--- src/common/options.js
+++ src/common/options.js
@@ -71,6 +71,7 @@ function onLoad() {
   // Restore previously set options (asynchronously)
   //
 
+  var optionsGetSuccessful = false;
   OptionsStore.get(function(prefs) {
     cssEdit.value = prefs['main-css'];
     cssSyntaxEdit.value = prefs['syntax-css'];
@@ -93,6 +94,8 @@ function onLoad() {
 
     // Start watching for changes to the styles.
     setInterval(checkChange, 100);
+
+    optionsGetSuccessful = true;
   });
 
   // Load the changelist section
@@ -124,6 +127,17 @@ function onLoad() {
       $('#tests-link').hide();
     }
   });
+
+  // Older Thunderbird may try to open this options page in a new ChromeWindow, and it
+  // won't work. So in that case we need to tell the user how they can actually open the
+  // options page. This is pretty ungraceful, but few users will encouter it, and fewer as
+  // time goes on.
+  setTimeout(function() {
+    if (!optionsGetSuccessful) {
+      alert('It looks like you are running an older version of Thunderird.\nOpen the Markdown Here Options via the message window Tools menu.');
+      window.close();
+    }
+  }, 500);
 
   loaded = true;
 }
